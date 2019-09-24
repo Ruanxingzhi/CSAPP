96dc81d63a521edd3a21063d9c7b255813f84f16
ruanxingzhi <ruanxingzhi@gmail.com>
2019-09-24 11:20:50 +0800

    写了一半作业

---
+++ Lab1/1180300105.c
@@ -0,0 +1,107 @@
+// 1180300105 吴雨伦
+// 作业题：2.58, 2.62, 2.66, 2.70, 2.74, 2.78, 2.82, 2.86 , 2.90, 2.94
+// 选做四星题：2.65
+
+#include <stdio.h>
+#include <limits.h>
+
+// 2.58
+int is_little_endian()
+{
+    union judger{int w;char c;}test={1};
+    return test.c;
+}
+
+// 2.62
+int int_shifts_are_arithmetic()
+{
+    int i=-1;
+    unsigned int u=-1;
+
+    return (u>>1) != (i>>1);
+}
+
+// 2.65 [选做四星题]
+int odd_ones(unsigned x)
+{
+    x = (x>>16) ^ x;
+    x = (x>>8) ^ x;
+    x = (x>>4) ^ x;
+    x = (x>>2) ^ x;
+    x = (x>>1) ^ x;
+    return x&1;
+}
+
+// 2.66
+int leftmost_one(unsigned x)
+{
+    x |= (x>>1);
+    x |= (x>>2);
+    x |= (x>>4);
+    x |= (x>>8);
+    x |= (x>>16);
+    return x^(x>>1);
+}
+
+// 2.70
+int fits_bits(int x,int n)
+{
+    // 题目已经约定，signed型变量的右移为算术右移。
+    int w = sizeof(int)<<3;
+    return x == ((x<<(w-n))>>(w-n));
+}
+
+// 2.74
+int tsub_ok(int x,int y)
+{
+    if(y == INT_MIN) return x<0;
+
+    int sum = x-y;
+    int neg_over = x<0 && y>0 && sum >=0;
+    int pos_over = x>= 0 && y<=0 && sum < 0;
+    return !neg_over && !pos_over;
+}
+
+// 2.78
+int divide_power2(int x,int k)
+{
+    int w = sizeof(int) << 3;
+    int bias = (x>>(w-1)) & ((1<<k)-1);
+    return (x+bias)>>k;
+}
+
+/*
+    2.82
+
+    A.  false
+        反例：x=0,y=1
+    B.  true
+        unsigned int值与模2**32意义下的加法，形成一个阿贝尔群。
+        由群论知识，不难推断两边恒等。
+    C.  true
+        左边 ≡ -x-1-y-1+1 ≡ -x-y-1      (mod 2**32)
+        右边 ≡ -(x+y)-1 ≡ -x-y-1        (mod 2**32)
+        故恒等。
+    D.  true
+        记inv(x)为x的模2**32意义下加法逆元。则
+        左边 ≡ x + inv(y)               (mod 2**32)             
+        右边 ≡ inv(y + inv(x)) ≡ inv(y) + inv(inv(x)) 
+            ≡ inv(y) + x               (mod 2**32)    
+        故恒等。
+    E.  true
+        最右边两位可能从1变成0. 这两位的权重是正值，故不会使x变得更大。
+*/
+
+int main(void)
+{
+    printf("%d\n",is_little_endian());
+    printf("%d\n",int_shifts_are_arithmetic());
+    printf("%d %d %d\n",odd_ones(123456),odd_ones(234567),odd_ones(345678));
+    printf("%#x\n",leftmost_one(0x6600));
+    printf("%d %d %d\n",fits_bits(-1000000,19),fits_bits(-1000000,20),fits_bits(-1000000,21));
+    printf("%d %d %d\n",fits_bits(0xAA,7),fits_bits(0xAA,8),fits_bits(0xAA,9));
+    printf("%d %d %d\n",tsub_ok(1,2),tsub_ok(1,INT_MIN),tsub_ok(2000000000,-1000000000));
+
+    printf("%d %d\n",divide_power2(35,3),divide_power2(-35,3));
+
+}
